369c046d97575731088340892f6a47a95fd47706
Thomas Rieu <rieu.thomas@hotmail.com>
2025-11-28 09:27:32 +0100

    new Equipment.tsx file + addition of Character.css

--- src/pages/Equipment.tsx
+++ src/pages/Equipment.tsx
@@ -1,6 +1,24 @@
+import Expedition from '../data/data.ts'
+import WeaponsList from '../component/WeaponsList.tsx'
+import SkillsList from "../component/SkillsList.tsx"
+import type { CharacterAboutProps } from '../interface/character.ts'
+
 function Equipment() {
+    const characters = Expedition.characterAbout
+
     return (
-        <h1>Hello Equipment</h1>
+        characters.map((character:CharacterAboutProps) => (
+            character.name !== 'Verso' && (
+                <>
+                    <h2>{character.name === 'Gustave' 
+                        ? `All Weapons for ${character.name } & Verso`
+                        : `All Weapons for ${character.name }`}</h2>
+                    <section className={`weapons ${character.name.toLowerCase()}-weapons`}>
+                        <WeaponsList weapons={character.weapons}/>
+                    </section>
+                </>
+            )     
+        ))
     )
 }
 
